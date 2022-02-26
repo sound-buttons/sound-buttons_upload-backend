aa435e198de366008e416e5d97b438e1e7b3e3d7
陳鈞 <jim60105@gmail.com>
2022-02-26 15:28:41 +0800

    Add intro icon discord and instagram

--- SoundButtons/SoundButtons.cs
+++ SoundButtons/SoundButtons.cs
@@ -600,6 +600,8 @@ namespace SoundButtons
             public string youtube { get; set; }
             public string twitter { get; set; }
             public string facebook { get; set; }
+            public string instagram { get; set; }
+            public string discord { get; set; }
             public string other { get; set; }
 
             public Link() { }
